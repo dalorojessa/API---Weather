c675db11d07c9948932b2e09a1a7fbf27a601195
jessa <daloro.jessa26@gmail.com>
2024-06-24 13:50:12 +0800

    updated

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -1,22 +1,32 @@
 <?php
 
+// Define the namespace for this controller class to organize the code
 namespace App\Http\Controllers;
 
+// Import the WeatherService 
 use App\Services\WeatherService;
+// Import the base controller class
 use Laravel\Lumen\Routing\Controller as BaseController;
 
+// Define the WeatherController class that extends the BaseController class
 class WeatherController extends BaseController
 {
+    // Define a protected property to hold an instance of weatherService
     protected $weatherService;
 
+    // Define the constructor from the WeatherService class
     public function __construct(WeatherService $weatherService)
     {
+        // Assign the received weatherService instance to the protected property
         $this->weatherService = $weatherService;
     }
 
+    // Define a method that will handle HTTP requests to show weather information for a given city
     public function show($city)
     {
+        // Call the getWeather method of the weatherService instance , passing the city name to retrieve the weather data
         $weather = $this->weatherService->getWeather($city);
+        // Return the weather data as a JSON
         return response()->json($weather);
     }
 }
--- app/Services/WeatherService.php
+++ app/Services/WeatherService.php
@@ -1,31 +1,46 @@
 <?php
 
+// Define the namespace for this service class to organize the code
 namespace App\Services;
 
+// Import the GuzzleHttp Client class for making HTTP requests
 use GuzzleHttp\Client;
 
+// Define the WeatherService class that will handle fetching weather data
 class WeatherService
 {
+    // Declare a protected property to hold an instance of the Guzzle HTTP client
     protected $client;
+    // Declare a protected property to hold the API key for the RapidAPI service
     protected $apiKey;
+    // Declare a protected property to hold the host URL for the weather API
     protected $host;
 
+    // Define the constructor for the WeatherService class
     public function __construct()
     {
+        // Initialize the Guzzle HTTP client
         $this->client = new Client();
+        // Retrieve the RapidAPI key from the env
         $this->apiKey = env('RAPIDAPI_KEY');
+        // Set the host URL for the weather API
         $this->host = 'open-weather13.p.rapidapi.com';
     }
 
+    // Define a method that fetches the weather data for a given city
     public function getWeather($city)
     {
+        // Send an HTTP GET request to the weather API using the Guzzle client
         $response = $this->client->request('GET', "https://{$this->host}/city/{$city}/EN", [
             'headers' => [
+                // Set API host header
                 'X-RapidAPI-Host' => $this->host,
+                // Set API key header
                 'X-RapidAPI-Key' => $this->apiKey,
             ],
         ]);
 
+        // Decode JSON response and return as array
         return json_decode($response->getBody()->getContents(), true);
     }
 }
